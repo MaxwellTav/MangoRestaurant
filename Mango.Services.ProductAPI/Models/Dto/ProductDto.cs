bdb6444774abfa3f315b9d0e561f6dead1684a95
Maxwell Tavares <maxwelltrt0709@gmail.com>
2023-04-14 15:35:59 -0400

    23. En veidad.

--- Mango.Services.ProductAPI/Models/Dto/ProductDto.cs
+++ Mango.Services.ProductAPI/Models/Dto/ProductDto.cs
@@ -2,5 +2,11 @@
 {
     public class ProductDto
     {
+        public int ProductId { get; set; }
+        public double Price { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string CategoryName { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
--- Mango.Services.ProductAPI/Models/Dto/ResponseDto.cs
+++ Mango.Services.ProductAPI/Models/Dto/ResponseDto.cs
@@ -1,6 +1,12 @@
-﻿namespace Mango.Services.ProductAPI.Models.Dto
+﻿using System.Collections.Generic;
+
+namespace Mango.Services.ProductAPI.Models.Dto
 {
     public class ResponseDto
     {
+        public bool IsSuccess { get; set; } = true;
+        public object Result { get; set; }
+        public string DisplayMessage { get; set; } = "";
+        public List<string> ErrorMessages { get; set; }
     }
 }
