2cd6ba343befb545ba726f60fb71ca45a8c5ef3e
Maxwell Tavares <maxwelltrt0709@gmail.com>
2023-04-14 15:33:53 -0400

    23. Create "Product Repository Interface".

--- Mango.Services.ProductAPI/Repository/IProductRepository.cs
+++ Mango.Services.ProductAPI/Repository/IProductRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace Mango.Services.ProductAPI.Repository
+﻿using Mango.Services.ProductAPI.Models.Dto;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Mango.Services.ProductAPI.Repository
 {
-    public interface IProductReposiroty
+    public interface IProductRepository
     {
+        Task<IEnumerable<ProductDto>> GetProducts();
+        Task<ProductDto> GetProductById(int ProductId);
+        Task<ProductDto> CreateUpdateProduct(ProductDto productDto);
+        Task<bool> DeleteProduct(int productId);
     }
 }
