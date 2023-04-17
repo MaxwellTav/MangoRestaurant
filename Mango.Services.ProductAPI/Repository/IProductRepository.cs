fe87df7fea29c59a3d3327921ccf8ab678fd5d89
Maxwell Tavares <maxwelltrt0709@gmail.com>
2023-04-17 15:35:26 -0400

    Actualizar la rama "(Maxwell)-Net-Core-5".

---
+++ Mango.Services.ProductAPI/Repository/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using Mango.Services.ProductAPI.Models.Dto;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Mango.Services.ProductAPI.Repository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductDto>> GetProducts();
+        Task<ProductDto> GetProductById(int ProductId);
+        Task<ProductDto> CreateUpdateProduct(ProductDto productDto);
+        Task<bool> DeleteProduct(int productId);
+    }
+}
