f5b2cececfceb1771c20271f9daac75dad5cd5a9
Maxwell Tavares <maxwelltrt0709@gmail.com>
2023-04-22 09:40:10 -0400

    32. Base service interface and API Request Model

---
+++ Mango.Web/Services/BaseService.cs
@@ -0,0 +1,21 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Services.IServices;
+using System.Threading.Tasks;
+
+namespace Mango.Web.Services
+{
+    public class BaseService : IBaseService
+    {
+        public ResponseDto responseModel { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+        public void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<T> SendAsync<T>(ApiRequest apiRequest)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Mango.Web/Services/IServices/IBaseService.cs
@@ -0,0 +1,12 @@
+﻿using Mango.Web.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace Mango.Web.Services.IServices
+{
+    public interface IBaseService : IDisposable
+    {
+        ResponseDto responseModel { get; set; }
+        Task<T> SendAsync<T>(ApiRequest apiRequest);
+    }
+}
