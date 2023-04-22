be24558029946cf29192c9b5821f67cde012338c
Maxwell Tavares <maxwelltrt0709@gmail.com>
2023-04-22 09:31:10 -0400

    31. Product repository interface and DTOs. Coping the "ProductDto.css". Coping the "ResponseDto.css" Adding "localhost" url in the Json file. Create and "SD" to the ".Web" project.

---
+++ Mango.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using Mango.Web.Models;
+using System.Threading.Tasks;
+
+namespace Mango.Web.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<T> GetAllProductsAsync<T>();
+        Task<T> GetProductByIdAsync<T>(int id);
+        Task<T> CreateProductAsync<T>(ProductDto productDto);
+        Task<T> UpdateProductAsync<T>(ProductDto productDto);
+        Task<T> DeleteProductAsync<T>(int id);
+    }
+}
