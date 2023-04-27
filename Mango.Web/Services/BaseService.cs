fd89af2736683f3d679a2ad550bac433e3cb9ef2
Maxwell Tavares <maxwelltrt0709@gmail.com>
2023-04-27 16:05:43 -0400

    33. Base service implementation.

--- Mango.Web/Services/BaseService.cs
+++ Mango.Web/Services/BaseService.cs
@@ -1,21 +1,53 @@
 ﻿using Mango.Web.Models;
 using Mango.Web.Services.IServices;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Reflection.Metadata;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Mango.Web.Services
 {
     public class BaseService : IBaseService
     {
-        public ResponseDto responseModel { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public ResponseDto responseModel { get; set; }
+        public IHttpClientFactory _httpClient;
 
-        public void Dispose()
+        public BaseService(IHttpClientFactory httpClient)
         {
-            throw new System.NotImplementedException();
+            responseModel = new ResponseDto();
+            _httpClient = httpClient;
         }
 
         public Task<T> SendAsync<T>(ApiRequest apiRequest)
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                var client = _httpClient.CreateClient("MangoAPI");
+                HttpRequestMessage message = new HttpRequestMessage();
+
+                message.Headers.Add("Accept", "application/json");
+                message.RequestUri = new Uri(apiRequest.Url);
+
+                client.DefaultRequestHeaders.Clear();
+
+                if (apiRequest.Data != null)
+                {
+                    message.Content = new StringContent(JsonConvert.SerializeObject(apiRequest.Data), 
+                        Encoding.UTF8, "application/json");
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(true);
         }
     }
 }
