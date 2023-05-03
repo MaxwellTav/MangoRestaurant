525a7fd2d3467c2546bec92e52895c486643ba81
Maxwell Tavares Rocha <Maxwelltrt0709@gmail.com>
2023-05-03 19:10:30 -0400

    54. Configuring APIScope Resources and Clients for Identity Server.
    "SD.cs" created in Mango.Services.Identity Proyect.

---
+++ Mango.Services.Identity/SD.cs
@@ -0,0 +1,41 @@
+﻿using Duende.IdentityServer.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks.Dataflow;
+
+namespace Mango.Services.Identity
+{
+    public static class SD
+    {
+        public const string Admin = "Admin";
+        public const string Customer = "Customer";
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>()
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Email(),
+                new IdentityResources.Profile()
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
+            {
+                new ApiScope("Mango", "Mango Server"),
+                new ApiScope(name: "read", displayName: "Read your data."),
+                new ApiScope(name: "write", displayName: "Write your data."),
+                new ApiScope(name: "delete", displayName: "Delete your data.")
+            };
+
+        public static IEnumerable<Client> Clients => new
+            List<Client>
+            {
+                new Client()
+                {
+                    ClientId = "client",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = { "read", "write", "profile" }
+                }
+        };
+    }
+}
